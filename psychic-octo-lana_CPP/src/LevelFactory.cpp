709284830e8fdfbe5d42e8260f27f46e19dea412
Pandu Raharja-Liu <raharjaliu@gmail.com>
2013-03-20 10:06:43 +0100

    trivial commit

--- psychic-octo-lana_CPP/src/LevelFactory.cpp
+++ psychic-octo-lana_CPP/src/LevelFactory.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 Level* LevelFactory::buildLevel1(){
-    Level* level1 = new Level("FMI Building", new vector<LevelObject>{}, new vector<Event>{}, new Condition(new vector<int>(0), new vector<int>(0), 0, 0));
+    Level* level1 = new Level("FMI Building", new vector<LevelObject>{}, new vector<Event>{}, new Condition(new vector<int>(), new vector<int>(0), 0, 0));
     return level1;
 }
 
