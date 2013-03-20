d57f29dd963b5d1d736eadd92d4138d2b17f5aca
Pandu Raharja-Liu <raharjaliu@gmail.com>
2013-03-20 03:16:09 +0100

    mod Game

--- psychic-octo-lana_CPP/src/Game.cpp
+++ psychic-octo-lana_CPP/src/Game.cpp
@@ -7,7 +7,7 @@ Game* Game::_instance = NULL; //initialize static member
 
 int main(){
     Game* game = Game::instance();
-
+    Item* verysexy = ItemFactory::produceItem(ItemFactory::PENIS);
     delete game;
 
     return 0;
